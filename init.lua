a7520c943b704922bd73a4b3a914c89f895000f9
elliot <elliot.blommaert@gmail.com>
2026-03-07 21:31:36 +0100

    add extend/shrink selection

--- init.lua
+++ init.lua
@@ -867,6 +867,23 @@ require('lazy').setup({
     end,
   },
 
+  {
+    {
+      'MeanderingProgrammer/treesitter-modules.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter' },
+      opts = {
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<A-o>',
+            node_incremental = '<A-o>',
+            scope_incremental = '<A-O>',
+            node_decremental = '<A-i>',
+          },
+        },
+      },
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
