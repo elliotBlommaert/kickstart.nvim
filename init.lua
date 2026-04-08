fdc6a2d0324f382dc8abb73347f2afbd9b4eeb9c
elliot <elliot.blommaert@vlaanderen.be>
2026-04-08 16:55:17 +0200

    stuff

--- init.lua
+++ init.lua
@@ -259,7 +259,7 @@ vim.api.nvim_create_autocmd('VimEnter', {
 })
 
 vim.filetype.add {
-  extension = { zsh = 'zsh' },
+  extension = { zsh = 'zsh', jinja = 'jinja', jinja2 = 'jinja', j2 = 'jinja' },
   filename = {
     ['.zshrc'] = 'zsh',
     ['.zshenv'] = 'zsh',
@@ -969,7 +969,59 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      -- mini.ai extends Vim's `a`/`i` textobjects.
+      --
+      -- Core concepts:
+      --   Operators:  d/c/y/v + a<key> or i<key>
+      --   Modifiers:  `n` = next, `l` = last (e.g. `valn` = select around next)
+      --   Count:      1 = nearest, 2 = surrounding, 3 = outer surrounding, ...
+      --               (e.g. `2vaF` selects the function *enclosing* the current one)
+      --   Motions:    `gù<key>` / `gµ<key>` jump to start/end of textobject
+      --               (e.g. `gùF` jump to start of current/previous function def)
+      --
+      -- Built-in textobjects (always available):
+      --   f  = function *call*  e.g. foo(…)   ← call site, not the definition
+      --   a  = argument in a function call
+      --   t  = HTML/XML tag
+      --   q  = quote  (' " ` |)
+      --   b  = balanced bracket  ()  []  {}  (alias for )/]/})
+      --   ), ], }, >, ' " ` = specific delimiters
+      --
+      -- Custom textobjects added below (via Treesitter):
+      --   F  = function *definition*  (outer = whole def, inner = body only)
+      --   c  = class
+      --   o  = conditional  (if/else/…)
+      --   L  = loop
+      --
+      -- Examples:
+      --   vaF      select Around current Function definition
+      --   viF      select Inside current Function definition (body only)
+      --   2vaF     select the Function definition *surrounding* the current one
+      --   daF      delete the whole function definition
+      --   gùF      jump to start of current/previous function definition
+      --   gµF      jump to end   of current/previous function definition
+      --   vanF     select Around Next Function definition
+      --   vif      select Inside current function Call arguments
+      --   daa      delete current function Argument (with comma)
+
+      local ai = require 'mini.ai'
+      ai.setup {
+        n_lines = 500,
+        mappings = {
+          goto_left = 'gù',
+          goto_right = 'gµ',
+        },
+        custom_textobjects = {
+          -- Function definitions (Treesitter-powered)
+          F = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+          -- Class definitions
+          c = ai.gen_spec.treesitter { a = '@class.outer', i = '@class.inner' },
+          -- Conditionals (if/else/switch/…)
+          o = ai.gen_spec.treesitter { a = '@conditional.outer', i = '@conditional.inner' },
+          -- Loops (for/while/…)
+          L = ai.gen_spec.treesitter { a = '@loop.outer', i = '@loop.inner' },
+        },
+      }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
@@ -998,9 +1050,10 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
     config = function()
       local filetypes = { 'zsh', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query',
-        'vim', 'vimdoc', 'python' }
+        'vim', 'vimdoc', 'python', 'jinja' }
       require('nvim-treesitter').install(filetypes)
       vim.api.nvim_create_autocmd('FileType', {
         pattern = filetypes,
@@ -1012,7 +1065,7 @@ require('lazy').setup({
   {
     {
       'MeanderingProgrammer/treesitter-modules.nvim',
-      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-treesitter/nvim-treesitter-textobjects' },
+      dependencies = { 'nvim-treesitter/nvim-treesitter' },
       opts = {
         incremental_selection = {
           enable = true,
@@ -1021,13 +1074,6 @@ require('lazy').setup({
             node_incremental = '<A-o>',
             scope_incremental = '<A-O>',
             node_decremental = '<A-i>',
-          },
-        },
-        textobjects = {
-          move = {
-            enable = true,
-            goto_next_start = { ['ùm'] = '@function.outer' },
-            goto_previous_start = { ['µm'] = '@function.outer' },
           },
         },
       },
