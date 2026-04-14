468bf84d8c5c86b0ebf51634098c3f15c12b26c6
elliot <elliot.blommaert@gmail.com>
2026-04-14 14:06:05 +0200

    Add oil.nvim

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,28 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = true,
+    columns = { 'icon', 'permissions', 'size', 'mtime' },
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 2,
+      max_width = 80,
+      max_height = 40,
+    },
+    keymaps = {
+      ['<C-p>'] = 'actions.preview',
+      ['<C-s>'] = false, -- don't shadow horizontal split
+      ['<C-h>'] = false, -- don't shadow window nav
+      ['l'] = 'actions.select',
+    },
+  },
+  keys = {
+    { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
+    { '<leader>o', '<cmd>Oil --float<cr>', desc = 'Open oil (float)' },
+  },
+}
