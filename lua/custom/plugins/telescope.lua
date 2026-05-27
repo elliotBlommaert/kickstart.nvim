6f2d717f1716b614623bce2eaa1bd0fed5f3027f
elliot <elliot.blommaert@vlaanderen.be>
2026-05-27 10:53:01 +0200

    stuff

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -51,7 +51,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
     vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = '[G]it [B]branches' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>sp', builtin.buffers, { desc = '[S]earch [P]revious buffers' })
 
     -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
     -- it is better explained there). This allows easily switching between pickers if you prefer using something else!
